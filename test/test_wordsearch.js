3ff07cd0e2717a77153d81860119ff7b8289f356
Iyris Vigil <iyrisvigil@gmail.com>
2020-08-27 08:40:53 +0000

    pair programming word search in 2D arrays

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -35,4 +35,64 @@ describe("#wordSearch()", function() {
 
     assert.isTrue(result);
   });
+
+  it("should return true if the word is present vertically", function() {
+    const result = wordSearch([
+      ['C', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['O', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['D', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['E', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'CODE')
+
+    assert.isTrue(result);
+  });
+
+  it("should return true if the word is backwards horizontally", function() {
+    const result = wordSearch([
+      ['E', 'D', 'O', 'C', 'Q', 'U', 'A', 'L'],
+      ['H', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['E', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['E', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'CODE')
+
+    assert.isTrue(result);
+  });
+  it("should return true if the word is diagonal", function() {
+    const result = wordSearch([
+      ['B','B','C'],
+      ['D','A','F'],
+      ['G','H','T']
+    ], 'BAT');
+
+    assert.isFalse(result);
+  });
+  it("should return true if the word is diagonal and backwards", function() {
+    const result = wordSearch([
+      ['T','B','C'],
+      ['D','A','F'],
+      ['G','H','C']
+    ], 'CAT');
+
+    assert.isFalse(result);
+  });
+
+  it("should return false if array is empty", function() {
+    const result = wordSearch([], 'CODE');
+    assert.isFalse(result);
+  });
+  it("should return false if array is null", function() {
+    const result = wordSearch(null, 'CODE');
+    assert.isFalse(result);
+  });
+
 });
--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,117 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
+//function receives a 2D array of letters and a word
+// The function must check to find the word horizontally and vertically
+//return true if the word is found and false if it isnt
+
+//=============FIRST OPTION
+// const wordSearch = (letters, word) => {
+//   let verticalJoin = [];
+//   const horizontalJoin = letters.map(ls => ls.join(''));
+
+//   for (let out = 0; out < letters[0].length; out++) {
+//     let horizontal = [];
+//     for (let inside = 0; inside < letters.length; inside++) {
+//       horizontal.push(letters[inside][out]);
+//     }
+    
+//     verticalJoin.push(horizontal.join(''));
+//   }
+
+//   if (((verticalJoin.join('')).includes(word)) || (horizontalJoin.join('')).includes(word)) {
+//     return true;
+//   }  return false;
+// }
+
+
+//============SECOND OPTION
+// const wordSearch = (letters, word) => {
+//     let verticalJoin = [];
+//     let horizontal = [];
+//     const horizontalJoin = letters.map(ls => ls.join(''));
+  
+//     for (let out = 0; out < letters.length; out++) {
+//       for (let inside = 0; inside < letters[out].length; inside++) {
+//           if(!horizontal) {
+//               horizontal = [];
+//           }
+//         horizontal.push(letters[inside][out]);
+//       }
+      
+//       verticalJoin.push(horizontal.join(''));
+//     }
+  
+//     if (((verticalJoin.join('')).includes(word)) || (horizontalJoin.join('')).includes(word)) {
+//       return true;
+//     }  return false;
+//   }
+
+
+//============THIRD OPTION INCLUDING STRETCH WORK 
+
+const wordSearch = (letters, word) => {
+  if (!letters) {
+      return false
+  }
+  if (letters.length === 0) {
+      return false;
+  }
+  const horizontalJoin = letters.map(ls => ls.join(''))
+  for (l of horizontalJoin) {
+      const array = l.split('');
+      const reverse = array.reverse();
+      const backwardsString = reverse.join('');
+      if (backwardsString.includes(word)) return true;
+  }
+
+  for (l of horizontalJoin) {
+      if (l.includes(word)) return true
+  }
+  //Vertical check
+  for (let col = 0; col < letters[0].length; col++) {
+      let verticalString = "";
+      for (let row of letters) {
+          verticalString += row[col];
+      }
+      if (verticalString.includes(word)) {
+          return true;
+      }
+      const array = verticalString.split('');
+      const reverse = array.reverse();
+      const backwardsString = reverse.join('');
+      if (backwardsString.includes(word)) {
+          return true;
+      }
+  }
+  const width = letters[0].length;
+  const height = letters.length;
+
+  let getDiagonal = (x, y) => {
+      let xIndex = x;
+      let yIndex = y;
+      let result = "";
+      while (xIndex < width && yIndex < height) {
+          result += letters[xIndex][yIndex];
+          xIndex++;
+          yIndex++;
+      }
+      if (result.includes(word)) {
+          return true;
+      }
+
+      let array = result.split();
+      array = array.reverse();
+      let reverseString = array.join('');
+      if (reverseString.includes(word)) {
+          return true;
+      }
+  }
+
+  for (let x = 0; x < width; x++) {
+      for (let y = 0; y < height; y++) {
+          getDiagonal(x, y);
+      }
+  }
+
+  return false;
 }
 
 module.exports = wordSearch
